a9e1c0ed48324c33fbe7169b3f2bc43bc4e998e6
Josue Flores <85895374+Josue320@users.noreply.github.com>
2021-09-28 11:57:25 -0600

    fin

--- Estructura/ActivoFijo.cs
+++ Estructura/ActivoFijo.cs
@@ -1,20 +1,31 @@
 ﻿using Estructura.Enum;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Estructura
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class ActivoFijo
     {
+        [JsonProperty]
         public int id { get; set; }
+        [JsonProperty]
         public string CodigoActivo { get; set; }
+        [JsonProperty]
         public string NombreActivo { get; set; }
+        [JsonProperty]
         public string Descripcion { get; set; }
+        [JsonProperty]
         public decimal ValorActivo { get; set; }
+        [JsonProperty]
         public decimal ValorResidual { get; set; }
+        [JsonProperty]
         public DateTime FechaAdquisicion { get; set; }
+        [JsonProperty]
         public TipoActivoFijo tipoActivoFijo { get; set; }
+        [JsonProperty]
         public MetodoDepreciacion metodoDepreciacion { get; set; }
     }
 }
